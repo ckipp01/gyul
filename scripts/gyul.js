3879ce091a0f159ac371b6c86ce11983b3b70213
ckipp01 <ckipp@pm.me>
2019-02-05 22:08:10 +0100

    small beginnings

---
+++ scripts/gyul.js
@@ -0,0 +1,15 @@
+'use strict'
+
+window.addEventListener('load', () => {
+  const createElem = (type, text) => {
+    const el = document.createElement(type)
+    const tn = document.createTextNode(text)
+
+    el.appendChild(tn)
+
+    return el
+  }
+
+  const h1 = text => createElem('h1', text)
+  document.body.appendChild(h1('gyul'))
+})
