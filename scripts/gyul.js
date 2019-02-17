a182c3eb09599e6e9499d0d1ce84814db5a2f819
ckipp01 <ckipp@pm.me>
2019-02-17 22:04:25 +0100

    cleanup

--- scripts/gyul.js
+++ scripts/gyul.js
@@ -13,7 +13,6 @@ class Gyul {
     this.tags = this.logs
       .flatMap(log => log.tags)
       .filter(log => log !== undefined)
-    this.view = 'info'
     this.template = retrieveTemplate(
       this.tree.template,
       this.tree.title,
@@ -23,85 +22,40 @@ class Gyul {
   package () {
     for (const section in this.template) {
       const sectionElem =
-        document.body.appendChild(createElem({ 'type': section }))
+        document.body.appendChild(createElem({ elem: { 'type': section } }))
       this.template[section]
-        .forEach(item => createElem({
-          type: item.type,
-          text: item.text,
-          attributes: item.attributes,
-          children: item.children,
-          parent: sectionElem
-        }))
+        .forEach(elem => createElem({ elem: elem, parent: sectionElem }))
     }
   }
 }
 
 const createElem = elemObject => {
-  const el = document.createElement(elemObject.type)
+  const elem = document.createElement(elemObject.elem.type)
 
-  if (elemObject.text) {
-    const tn = document.createTextNode(elemObject.text)
-    el.appendChild(tn)
+  if (elemObject.elem.text) {
+    const textNode = document.createTextNode(elemObject.elem.text)
+    elem.appendChild(textNode)
   }
 
-  if (elemObject.attributes) {
-    elemObject.attributes
-      .map(attribute => el.setAttribute(attribute.type, attribute.value))
+  if (elemObject.elem.attributes) {
+    elemObject.elem.attributes
+      .map(attribute => elem.setAttribute(attribute.type, attribute.value))
   }
 
-  if (elemObject.children) {
-    elemObject.children
-      .map(child => {
-        createElem({
-          type: child.type,
-          text: child.text,
-          attributes: child.attributes,
-          children: child.children,
-          parent: el
-        })
-      })
+  if (elemObject.elem.children) {
+    elemObject.elem.children
+      .forEach(childElem => createElem({ elem: childElem, parent: elem }))
   }
 
   if (elemObject.parent) {
-    elemObject.parent.appendChild(el)
+    elemObject.parent.appendChild(elem)
   } else {
-    return el
+    return elem
   }
-}
-
-const createAndAttatch = async (elemObject, sectionElem) => {
-  let elem
-  switch (elemObject.type) {
-    case 'h1':
-    case 'h2':
-    case 'p':
-    case 'img':
-      elem = await createElem(
-        { type: elemObject.type, text: elemObject.text, attributes: elemObject.attributes }
-      )
-      break
-    case 'div':
-      elem = await createElem(
-        { type: elemObject.type, text: elemObject.text, children: elemObject.children, attributes: elemObject.attributes }
-      )
-      break
-  }
-  sectionElem.appendChild(elem)
 }
 
 const retrieveTree = key => CRATE[key] ? CRATE[key] : CRATE.missing
-const showInfo = () => {
-  const main = document.getElementsByTagName('main')[0]
-  main.innerHTML = ''
-  GYUL.template.main
-    .forEach(elem => createElem({
-      type: elem.type,
-      text: elem.text,
-      attributes: elem.attributes,
-      children: elem.children,
-      parent: main
-    }))
-}
+
 const retrieveTemplate = (template, title, body) => {
   const t = TEMPLATES[template]
   return t(title, body)
@@ -111,15 +65,25 @@ const retrieveStats = (acc, cv) => {
   acc.totalTime += Number(cv.time)
   return acc
 }
+
+const showInfo = () => {
+  const main = document.getElementsByTagName('main')[0]
+  main.innerHTML = ''
+  GYUL.template.main
+    .forEach(elem => createElem({ elem: elem, parent: main }))
+}
+
 const showStats = () => {
   const main = document.getElementsByTagName('main')[0]
   main.innerHTML = `<p>Total Entries: ${GYUL.stats.totalEntries}</p><p>Total Time Spent: ${GYUL.stats.totalTime}</p>`
 }
+
 const showLogs = () => {
   const main = document.getElementsByTagName('main')[0]
   const logNotes = GYUL.logs.map(log => `<p>${log.notes}</p>`)
   main.innerHTML = logNotes.join('')
 }
+
 const showTags = () => {
   const main = document.getElementsByTagName('main')[0]
   const tagNames = GYUL.tags.map(tag => `<p>${tag}</p>`)
